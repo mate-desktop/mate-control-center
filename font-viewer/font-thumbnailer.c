17dccaa7c69f0ab8e278699a5d138662ce8126b5
infirit <infirit@gmail.com>
2014-12-16 13:11:43 +0100

    Revert "Add back support for Glib 2.32"
    This reverts commit 5ee2c81ca6d335b18d51dbc6f1036c5f08396220.

--- font-viewer/font-thumbnailer.c
+++ font-viewer/font-thumbnailer.c
@@ -256,10 +256,6 @@ main(int argc, char **argv)
 
     setlocale (LC_ALL, "");
 
-#if !GLIB_CHECK_VERSION (2, 36, 0)
-    g_type_init ();
-#endif
-
     context = g_option_context_new (NULL);
     g_option_context_add_main_entries (context, options, GETTEXT_PACKAGE);
 
