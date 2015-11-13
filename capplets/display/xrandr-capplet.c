mate-desktop/mate-control-center

commit c6935f255ea711557c3c5e0e3ae8439e59c9a7aa

Display settings: Show output names in addition to display names.

capplets/display/xrandr-capplet.c
@@ -1653,20 +1653,25 @@ static PangoLayout *
 get_display_name (App *app,
 		  MateRROutputInfo *output)
 {
-    const char *text;
+    char *text;
+    PangoLayout * layout;
 
     if (mate_rr_config_get_clone (app->current_configuration)) {
-	/* Translators:  this is the feature where what you see on your laptop's
-	 * screen is the same as your external monitor.  Here, "Mirror" is being
-	 * used as an adjective, not as a verb.  For example, the Spanish
-	 * translation could be "Pantallas en Espejo", *not* "Espejar Pantallas".
-	 */
-	text = _("Mirror Screens");
-    } else
-	text = mate_rr_output_info_get_display_name (output);
-
-    return gtk_widget_create_pango_layout (
-	GTK_WIDGET (app->area), text);
+    /* Translators:  this is the feature where what you see on your laptop's
+     * screen is the same as your external monitor.  Here, "Mirror" is being
+     * used as an adjective, not as a verb.  For example, the Spanish
+     * translation could be "Pantallas en Espejo", *not* "Espejar Pantallas".
+     */
+        text = _("Mirror Screens");
+    } 
+    else {
+        text = g_strdup_printf ("%s\n<small>%s</small>", mate_rr_output_info_get_display_name (output), mate_rr_output_info_get_name (output));
+    }
+    layout = gtk_widget_create_pango_layout (GTK_WIDGET (app->area), text);
+    pango_layout_set_markup (layout, text, -1);
+    g_free (text);
+    pango_layout_set_alignment (layout, PANGO_ALIGN_CENTER);
+    return layout;
 }
 
 static void

commit 93ceed58e923c41814b05524c02faf03adf3d761

display properties: don't try to free a string literal (fix crash)
crash introduced in https://github.com/mate-desktop/mate-control-center/commit/c6935f255ea711557c3c5e0e3ae8439e59c9a7aa

capplets/display/xrandr-capplet.c
@@ -1665,7 +1665,7 @@ get_display_name (App *app,
      * used as an adjective, not as a verb.  For example, the Spanish
      * translation could be "Pantallas en Espejo", *not* "Espejar Pantallas".
      */
-        text = _("Mirror Screens");
+        text = g_strdup_printf (_("Mirror Screens"));
     } 
     else {
         text = g_strdup_printf ("<b>%s</b>\n<small>%s</small>", mate_rr_output_info_get_display_name (output), mate_rr_output_info_get_name (output));